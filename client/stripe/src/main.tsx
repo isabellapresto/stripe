f4db55fc25f173b819c4f98e66c0ecefe740db6c
isabellapresto <isabella.presto@gmail.com>
2023-09-07 14:23:29 +0200

    checkout funkar

--- client/stripe/src/main.tsx
+++ client/stripe/src/main.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Confirmation from "./components/Confirmation.tsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='confirmation' element={<Confirmation />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
